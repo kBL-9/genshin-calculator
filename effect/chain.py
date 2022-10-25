8213d250c69f840ed54e0f8cb7f787bcd6177458
kBL_ <102797159+kBL-9@users.noreply.github.com>
2022-10-25 16:30:55 +1100

    temp

--- effect/chain.py
+++ effect/chain.py
@@ -6,16 +6,20 @@ class ChainEffect:
         self.name = name
         
     # duration in seconds
-    def add_effect_to_chain(self, effect: Effect, duration: float):
-        self.chain.append((effect, duration))
+    def add_effect_to_chain(self, effect: Effect):
+        self.chain.append(effect)
         return self
+
+    def apply_effects(self, team: Team, enemy: Enemy, starting_time: int = 0):
+        for effect in self.chain:
+            effect.apply_effect(team, enemy, starting_time)
         
     def to_str(self, tabs = 0):
         result = "Chain \"{0}\" consists of following effects:\n".format(self.name)
         count = 0
-        for (effect, duration) in self.chain:
+        for effect in self.chain:
             result += "  " * tabs
-            result += "Chain {0} for {1}s:\n{2}".format(count, self.duration, self.effect.to_str(tabs + 1))
+            result += "Chain {0} for {1}s:\n{2}".format(count, effect.duration, effect.to_str(tabs + 1))
             count += 1
             
         return result
--- effect/effect.py
+++ effect/effect.py
@@ -1,9 +1,34 @@
-
+from character import Character
+from enemy import Enemy
+from team import Team
 
 class Effect:
     def __init__(self):
-        pass
-        
+        self.duration = -1
+        self.name = ""
+        self.source = None
+        self.callback = lambda: 0
+
+    def set_name(self, name: str):
+        self.name = name
+        return self
+
+    def set_effect_applicator(callback: Callable[[Character, Team, Enemy, int], int]):
+        self.callback = callback
+        return self
+
+    def set_source(self, source: Character):
+        self.source = source
+        return self
+
+    def apply_effect(
+        self,
+        team: Team,
+        enemy: Enemy,
+        starting_time: int = 0):
+        self.callback(self.source, team, enemy, self.duration)
+        return starting_time + max(0, self.duration)
+
     def to_str(self, tabs = 0):
         result = ""
         return result
